9d65e08d1c416013f076e4f7935ff9f5d580857e
itanabojovic <62520977+itanabojovic@users.noreply.github.com>
2024-04-10 20:28:33 +0200

    Delete fetch_fastas.sh

--- fetch_fastas.sh
+++
@@ -1,6 +0,0 @@
-for i in P99999 P00004 P0C0X8 P00091 Q93VA3
-do
-  wget https://www.uniprot.org/uniprot/$i.fasta
-done
-
-cat P99999.fasta P00004.fasta P0C0X8.fasta P00091.fasta Q93VA3.fasta > cytc_aln.clw
